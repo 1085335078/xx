4f65d489125883af51e3248c2ae465597216dffe
mldn <mldnqa@163.com>
2018-08-09 14:39:59 +0800

    create new file;

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test{
 	public static void main(String args[]){
-		System.out.println("大家好");
+		System.out.println("哎哟我哈哈      年后");
+		System.out.println("哎哟我哈哈      年后");
+		System.out.println("哎哟我哈哈      年后");
+		System.out.println("哎哟我哈哈      年后");
+		System.out.println("哎哟我哈哈      年后");
+		
 	}
 }
