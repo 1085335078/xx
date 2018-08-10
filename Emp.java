589ebceb1a55981062868289f2d8ffe930afd5a6
mldn <mldnqa@163.com>
2018-08-11 01:36:23 +0800

    Add Dept.java AND Change Emp.java

--- Emp.java
+++ Emp.java
@@ -5,7 +5,9 @@ public class Test{
 		System.out.println("2");
 		System.out.println("2");
 		System.out.println("2");
-		System.out.println("2");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
 		
 		
 	}
